a6ec671be3ac5abdad859020593e480c88d899cd
Tangir <tangir.miah@gmail.com>
2020-11-25 00:44:25 +0000

    correction on particle background

--- src/pages/Sections/HomeSection.js
+++ src/pages/Sections/HomeSection.js
@@ -2,7 +2,6 @@ import React from "react"
 import classes from "./HomeSection.module.css"
 import Hero from "../../components/Hero/Hero"
 import Particles from "react-particles-js"
-// import ParticlesBg from "particles-bg"
 
 const HomeSection = () => {
   return (
@@ -10,28 +9,25 @@ const HomeSection = () => {
       <Hero />
       <Particles
         className={classes.ParticleBg}
-        // style={{
-        //   opacity: "50%",
-        // }}
         params={{
           particles: {
             number: {
               value: 200,
+              density: {
+                enable: true,
+                area: 250,
+                factor: 5000,
+              },
             },
             color: {
               value: "#f8f8f8",
             },
             interactivity: {
-              detectsOn: "parent",
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
+              resize: true,
             },
           },
         }}
       />
-      {/* <ParticlesBg color={"#f8f8f8"} num={100} type="cobweb" bg={true} /> */}
     </div>
   )
 }
