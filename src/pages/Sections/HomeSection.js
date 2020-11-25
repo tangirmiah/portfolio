53f63f131e2e317ed731a724175a4444abf05cc2
Tangir <tangir.miah@gmail.com>
2020-11-25 13:46:22 +0000

    particles update

--- src/pages/Sections/HomeSection.js
+++ src/pages/Sections/HomeSection.js
@@ -8,9 +8,18 @@ const HomeSection = () => {
     <div className={classes.Home} id="HomeSection">
       <Hero />
       <Particles
-        className={classes.ParticleBg}
         params={{
           particles: {
+            size: {
+              value: 4,
+              anim: {
+                enable: true,
+                speed: 20,
+              },
+            },
+            color: {
+              value: "#f8f8f8",
+            },
             number: {
               value: 200,
               density: {
@@ -19,14 +28,32 @@ const HomeSection = () => {
                 factor: 5000,
               },
             },
-            color: {
-              value: "#f8f8f8",
-            },
-            interactivity: {
-              resize: true,
-            },
           },
+          interactivity: {
+            detectsOn: "window",
+            events: {
+              onHover: {
+                enable: true,
+                mode: ["grab"],
+              },
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+            },
+            modes: {
+              grab: {
+                distance: 200,
+                line_linked: {
+                  opacity: 2,
+                },
+              },
+            },
+            resize: true,
+          },
+          retina_detect: true,
         }}
+        className={classes.ParticleBg}
       />
     </div>
   )
