eb78ff8800eddeef06e160080b97efce262e8cd3
Tangir <tangir.miah@gmail.com>
2021-01-01 22:24:55 +0000

    image meta check

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,39 @@ import ProjectsSection from "./Sections/ProjectsSection"
 import ContactSection from "./Sections/ContactSection"
 import Helmet from "react-helmet"
 import og from "../assets/logo/Tangir_32x32.png"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function Home() {
+  const data = useStaticQuery(graphql`
+    query {
+      icon: allFile(filter: { name: { eq: "Tangir_32x32" } }) {
+        edges {
+          node {
+            publicURL
+            name
+          }
+        }
+      }
+    }
+  `)
+  console.log(data.icon.edges[0].node.publicURL)
   return (
     <>
       <Helmet>
         <title>Tangir Miah Portfolio</title>
-        <meta property="og:url" content="https://tangir.dev/"></meta>
-        <meta property="og:title" content="Portfolio Tangir Miah"></meta>
-        <meta property="og:site_name" content="tangir.dev"></meta>
+        <meta name="author" content="Tangir Ahammed Miah" />
+        <meta property="og:url" content="https://tangir.dev/" />
+        <meta property="og:title" content="Portfolio Tangir Miah" />
+        <meta property="og:site_name" content="tangir.dev" />
         <meta
           property="og:description"
           content="This is my portfolio where i showcase my projects and you can learn something about me"
-        ></meta>
-        <meta property="og:image" content={og} data-rh="true"></meta>
+        />
+        <meta
+          property="og:image"
+          content={data.icon.edges[0].node.publicURL}
+          data-rh="true"
+        />
       </Helmet>
       <Layout>
         <HomeSection />
